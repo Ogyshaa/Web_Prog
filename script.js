33c8794c84086285c910ffcd7af741bb6c9f19dc
Ogyshaa <ognjenradojkovic7@gmail.com>
2024-11-01 00:56:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,125 +1,190 @@
-class Kerina{
-    constructor(parametar1,parametar2){
-        this.parametar1=parametar1;
-        this.parametar2=parametar2;
-    }
-
-
-}
-
-let objekat = new Kerina(12,"Kuče");
-
-function pas1(ime,rasa,tezina,boja)
+// naziv klase
+class ImeKlase
 {
-    this.ime=ime;
-    this.rasa=rasa;
-    this.tezina=tezina;
-    this.boja=boja;
+    constructor (parametarkonstrukotra1, parakon)
+    {
+        this.parametarkonstrukotra1 = parametarkonstrukotra1;
+        this.parakon = parakon;
+    } 
 }
 
-let god1= new pas1("Džeki","Pudlica",2,"bela");
+let obj = new ImeKlase("para1", "para2");
+
+function pas1(ime, rasa, tezina, boja)
+{
+    this.ime = ime ;
+    this.rasa = rasa;
+    this.tezina = tezina;
+    this.boja = boja;
+}
+
+let dog1 = new pas1("dzeki", "majmun", "50kg", " gorila");
 
 class Pas{
-    constructor(ime,rasa,tezina,boja)
+    constructor(ime, rasa, tezina, boja)
     {
-    this.ime=ime;
-    this.rasa=rasa;
-    this.tezina=tezina;
-    this.boja=boja;
+        this.ime = ime ;
+    this.rasa = rasa;
+    this.tezina = tezina;
+    this.boja = boja;
     }
 }
+let dog  = new Pas("dzeki", "majmun", "50kg", " gorila")
 
-let dog=new Pas("Džeki","Pudlica",2,"bela");
-
-console.log("Hej, danas sam dobila psa. Zove se:",dog.ime);
+console.log("Bla bla bla", dog.rasa );
 
 class Covek{
-    constructor(ime,prezime)
+    constructor(ime, prezime)
     {
-        this.ime=ime;
-        this.prezime=prezime;
+        this.ime = ime;
+        this.prezime = prezime;
     }
 }
 
-let persona=new Covek("David","Milosevic");
-console.log("danas je",persona.ime,"seo u prvu klupu,",persona.ime,persona.prezime);
-let osoba1 = new Covek("Damnjan");
-console.log(osoba1);
+let cuvek = new Covek("Goran", "Sindjelic")
+console.log("Danas je ",cuvek.ime,"jeo sarmu");
+
+let osoba = new Covek("Dino"); // drugi parametar undefinde
+console.log(osoba);
 
 class Covek1{
-    constructor(ime,prezime="Savic")
+    constructor(ime='Slavica' ,prezime='Savic')
     {
-        this.ime=ime;
-        this.prezime=prezime;
+        this.ime = ime;
+        this.prezime = prezime;
     }
-}
-let osoba2= new Covek1("Djordje");
-
-console.log(osoba2);
-
-let osoba3 = new Covek1("Djordje","Nedimovic");
-console.log(osoba3);
-/*class Covek2{
-    constructor(ime,prezime)
+    ispis()
     {
-        this.ime=ime;
-        this.prezime=prezime;
-    }
-    pozdrav(){
-    conos.log("cao"+this.ime)
-    };
-}
-*/
-//let osoba4 = new Covek2("Pera","Peric");
-//Ognjenosoba4.pozdrav();
-
-/*napraviti klasu sa nazivom osoba i dodeliti joj konstruktor za ime i  prezime
-kreirati promenljivu i dodeliti vrednost novom objektu objektu osoba koristeci ime i prezime
-dugara pored sebe
-kreirati jos jednu promenljivu drugara iza sebe, uneti imena oba prijatelja u kozolu sa pozdarvom zdravo*/
-
-/*class Osoba{
-    constructor(ime, prez){
-        this.ime=ime;
-        this.prez=prez;
-    }
-
-    pozz(){
-        console.log("Cao",this.ime,this.prez);
+        console.log('Hi bitch')
     }
 }
-let drug1=new Osoba(prompt("Unesi ime prvog"),prompt("Sad prezime"));
-drug1.pozz();
-let drug2=new Osoba(prompt("Unesi ime drugog"),prompt("Sad prezime"));
-drug2.pozz();
 
-//koristeci klasu koju ste napravil dodati metod koji se zove puno ime i prezime
-//koji vraca vrednosti ime i prezime kada ga pozovete
-*/
-class Osoba1{
-    constructor(ime, prez){
-        this.ime=ime;
-        this.prez=prez;
-    }
+let osoba2 = new Covek1("MAREEEEEEEEEEEEEEEE");
+console.log(osoba2.ime, osoba2.prezime);
+osoba2.ispis();
+let osoba3 = new Covek1("MAreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee","Nikolic");
+console.log(osoba3.ime, osoba3.prezime);
 
-    pozz(){
-        console.log("Cao",this.ime,this.prez);
-    }
-    puno_ime_prezime(drug,drug3)
+class Covek2{
+    constructor(ime, prez)
     {
-        return drug + ' ' + drug3;
+        this.ime =  ime;
+        this.prez = prez;
+    }
+    pozdrav()
+    {
+        console.log('cao '+ this.ime);
+    }
+}
+let osboa4 = new Covek2("prea", "peric")
+osboa4.pozdrav();
+
+/*Napraviti klasu sa nazivom osoba i dodeliti joj konstruktor za ime i prezime 
+keirati promenjivu i dodeliti vrednost novom objektu osoba koristici ime i prezime 
+drugra pored sebe
+kreirati jos jednu promenlivu drguraga iza sebe
+uneti imena oba priljatelja u konzolu sa pozdravom zdravo*/
+
+class Coek{
+    constructor(ime , prez )
+    {
+        this. ime = ime;
+        this.prez = prez;
+    }
+    poz()
+    {
+        console.log("Vozdra " + this.ime +' '+ this.prez);
+    }
+    PunoImePrezime(ime , prezime)
+    {
+        return ime + ' ' + prezime;
+    }
+}
+let ime = prompt();
+let prezime = prompt();
+
+let drug1 = new Coek(ime, prezime);
+
+let ime1 = prompt();
+let prezime1 = prompt();
+
+let drug2 = new Coek(ime1, prezime1);
+
+drug1.poz();
+drug2.poz();
+
+let imence = drug1.PunoImePrezime(ime ,prezime);
+console.log(imence);
+
+let imence1 = drug2.PunoImePrezime(ime1 ,prezime1);
+console.log(imence1);
+//korisiteci prethodnu klasu dodati metod koji se zove PunoImePrezime 
+//koji vraca vred ime i prezime kada ga pozovete
+
+class Coek1{
+    #ime ;
+    #prezime ; 
+    constructor(ime , prezime)
+    {
+        this.#ime = ime ;
+        this.#prezime =prezime ;
+    }
+    get ime(){
+        return this.#ime;
+    } 
+    set ime (ime){
+        this.#ime = ime;
+    }
+    get prezime(){
+        return this.#prezime;
+    } 
+    set prezime (prezime){
+        this.#prezime = prezime;
     }
 }
 
-let ime1=prompt("Unesi ime prvog");
-let prez1=prompt("Unesi prez");
+let p = new Coek1("Ksenija", " Tufonic");
+console.log(p.ime, p.prezime);
 
-let ime2=prompt("Unesi ime drugog");
-let prez2=prompt("Unesi prezime");
+class Vozila{
+    constructor(boja, trBrz, maxBrz)
+    {
+        this. boja = boja;
+        this. trBrz = trBrz;
+        this. maxBrz = maxBrz;
+    }
+    kretanje()
+    {
+        console.log('Krecete se brzinom', this.trBrz);
+    }
+    ubrzanje(brzina)
+    {
+        this.trBrz += brzina;
+    }
+}
+class Motori extends Vozila{
+    constructor(boja ,trBrz, maxBrz, gorivo){
+        super(boja, trBrz, maxBrz);
+        this.gorivo = gorivo;
+    }
+    voznjaToc()
+    {
+        console.log('Vozite na JEDNOMM tocku :)');
+    }
+}
 
-let covek10 =new Osoba1(ime1,prez1);
-let puno = covek10.puno_ime_prezime(ime1 , prez1);
-console.log(puno);
-let covek11 = new Osoba1(ime2,prez2);
-let puno1 = covek11.puno_ime_prezime(ime2,prez2);
-console.log(puno1);
+let motor = new Motori("crna", 0, 250, 'benzin');
+console.log(motor.gorivo);
+motor.ubrzanje(130);
+motor.kretanje();
+
+Covek.prototype.uvesti = function()
+{
+    console.log("Hej ja sam", this.ime);
+}
+Covek.prototype.omiljenjaBoja = 'Plavicasta';
+
+let p1= new Covek('Janja', 'Velebit');
+console.log(p1.omiljenjaBoja);
+
+p1.uvesti();
