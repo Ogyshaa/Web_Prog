9412da27712ff238e91123bcd931da4368f438cd
NemanjaKing <anjamen213@gmail.com>
2025-01-30 10:24:18 +0100

    Add files via upload

---
+++ 30.01 izmenjeno/script.js
@@ -0,0 +1,55 @@
+//odabir elemenata kao objekta
+//kako mozemo da dodjemo do taga odnosno elementa koji je paragraf krecuci se kroz DOM - objektni domen dokumenta
+
+console.log(document.body.children.pozdrav);
+
+document.body.children.pozdrav.innerText="Dovidjenja";
+
+document.body.children.pozdrav.innerHTML="<b>Dovidjenja</b>";
+
+//pristup elementima
+
+//pomocu ID-a
+//prema naziva klase
+//prema nazivu taga
+//pomocu CSS selektora
+
+console.log(document.getElementById("drugi"));
+console.log(document.getElementsByTagName("div").item(1)); //html kolekcija
+console.log(document.getElementsByTagName("div").namedItem("prvi"));
+console.log(document.getElementsByClassName("nesto"));
+
+//pristup pomocu metoda queryselector i queryselectorAll
+console.log(document.querySelector('p')); //svi elementi taga p
+console.log(document.querySelector(".primer"));
+console.log(document.querySelector("#prvi"));
+
+console.log(document.querySelectorAll('p'));
+console.log(document.querySelectorAll(".primer"));
+console.log(document.querySelectorAll("#drugi"));
+
+//element hendler klikova - onclick()
+
+function stop(){
+    alert("Ups!");
+}
+
+document.getElementById("druginacin").onclick = function(){
+    alert("Oh ne, upsic");
+}
+
+//manipulisanje stilovima elemenata
+//sakrivanje - display svojstvo se postavi na none
+//vidljivost - display svojstvo se postavi na block
+
+function svojstvoDisplay(){
+    let p= document.getElementById("magija");
+    if(p.style.display === "none")
+    {
+        p.style.display = "block";
+    }
+    else
+    {
+        p.style.display = "none";
+    }
+}
