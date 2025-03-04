a5e53b072e461d1e903695944d3002ee236e07d6
Ogyshaa <ognjenradojkovic7@gmail.com>
2025-03-04 22:49:30 +0100

    Add files via upload

---
+++ CONTRLONI CETVTAK😭/zadacisakontrolngo/kvadvat u kvug bez kanvasa/scrpt.js
@@ -0,0 +1,6 @@
+function promeniatribu(){
+    let el = document.getElementById('shape')
+    el.setAttribute('style', 'background-color:red;border:1px solid black')
+    el.setAttribute('id','new')
+    el.setAttribute('class','circle')
+}
